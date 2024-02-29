231711333b20c1227735e54609b0c2af8728312b
Martin <martin@hotmail.com.br>
2024-02-29 14:45:38 -0300

    feat: updated impl of redislock with dropbomb
    Signed-off-by: Martin <martin@hotmail.com.br>

--- src/infrastructure/redis_lock.rs
+++ src/infrastructure/redis_lock.rs
@@ -1,11 +1,14 @@
 use compact_str::CompactString;
-use redis::{Client, Value};
+use drop_bomb::DropBomb;
+use redis::aio::{ConnectionLike, ConnectionManager};
+use redis::{
+    AsyncCommands, Client, Commands, ExistenceCheck, RedisResult, SetExpiry, SetOptions, Value,
+};
 use std::iter::repeat_with;
-use std::sync::Arc;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct RedisLock {
-    rconn: Arc<Client>,
+    rconn: ConnectionManager,
     /// A random alphanumeric string
     resource: CompactString,
     /// Value the lock is tied to
@@ -13,69 +16,92 @@ pub struct RedisLock {
     ttl_max: usize,
 }
 
-#[derive(Debug, Clone)]
 pub struct RedisLockGuard<'a> {
     lock: &'a RedisLock,
+    bomb: DropBomb,
 }
 
-impl<'a> Drop for RedisLockGuard<'a> {
-    fn drop(&mut self) {
-        futures::executor::block_on(async { self.lock.unlock().await });
+impl<'a> RedisLockGuard<'a> {
+    pub async fn release(mut self) {
+        self.lock.unlock().await.unwrap();
+        self.bomb.defuse();
     }
 }
 
 impl RedisLock {
-    pub fn new(rconn: impl Into<Arc<Client>>, val: i32, ttl_max: usize) -> RedisLock {
+    pub fn new(rconn: ConnectionManager, val: i32, ttl_max: usize) -> RedisLock {
         let resource: CompactString = repeat_with(fastrand::alphanumeric).take(5).collect();
 
         Self {
-            rconn: rconn.into(),
+            rconn,
             resource,
             val,
             ttl_max,
         }
     }
     pub async fn acquire(&self) -> Result<RedisLockGuard, ()> {
-        if lock(&self.rconn, &self.resource, self.val, self.ttl_max).await {
-            Ok(RedisLockGuard { lock: self })
-        } else {
-            return Err(());
+        let mut retries = 0;
+        loop {
+            if retries > 10 {
+                return Err(());
+            }
+
+            match lock(self.rconn.clone(), &self.resource, self.val, self.ttl_max).await {
+                true => {
+                    return Ok(RedisLockGuard {
+                        lock: self,
+                        bomb: DropBomb::new("RedisLockGuard must be released with release."),
+                    });
+                }
+                false => tokio::task::yield_now().await,
+            };
+            retries += 1;
         }
     }
 
-    async fn unlock(&self) -> Result<(), ()> {
-        let res = drop_lock(&self.rconn, &self.resource, self.val).await;
-        Ok(())
+    async fn unlock(&self) -> Result<bool, ()> {
+        let res = drop_lock(self.rconn.clone(), &self.resource, self.val).await;
+        Ok(res)
     }
 }
 
-async fn lock(redis: &Client, resource: &str, val: i32, ttl: usize) -> bool {
-    let mut conn = redis.get_tokio_connection().await.unwrap();
-    let result = redis::cmd("SET")
-        .arg(resource)
-        .arg(val)
-        .arg("NX")
-        .arg("PX")
-        .arg(ttl)
-        .query_async::<_, Value>(&mut conn)
-        .await
-        .unwrap();
-    matches!(result, Value::Okay)
-}
+async fn lock(
+    mut redis: impl ConnectionLike + AsyncCommands,
+    resource: &str,
+    val: i32,
+    ttl: usize,
+) -> bool {
+    let options = SetOptions::default()
+        .conditional_set(ExistenceCheck::NX)
+        .with_expiration(SetExpiry::PX(10000));
 
-async fn drop_lock(redis: &Client, resource: &str, val: i32) -> bool {
-    let mut connection = redis.get_tokio_connection().await.unwrap();
-    let script = redis::Script::new(DROP_SCRIPT);
-    let res = script
-        .key(resource)
-        .arg(val)
-        .invoke_async::<_, Value>(&mut connection)
+    let res = redis
+        .set_options::<_, _, Value>(resource, val, options)
         .await
         .unwrap();
     matches!(res, Value::Okay)
 }
 
-const DROP_SCRIPT: &str = r#"if redis.call("get", KEYS[1]) == ARGV[1] then
+async fn drop_lock(
+    mut conn: impl ConnectionLike + AsyncCommands,
+    resource: &str,
+    val: i32,
+) -> bool {
+    let script = redis::Script::new(DROP_SCRIPT);
+    let res = script
+        .key(resource)
+        .arg(val)
+        .invoke_async::<_, i32>(&mut conn)
+        .await;
+
+    match res {
+        Ok(val) => val == 1,
+        Err(_) => false,
+    }
+}
+
+const DROP_SCRIPT: &str = r#"
+    if redis.call("get", KEYS[1]) == ARGV[1] then
         return redis.call("del", KEYS[1])
     else
         return 0
