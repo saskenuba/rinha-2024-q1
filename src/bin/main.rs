7c818d2be786f67af6c0e037c8cc094264d61071
Martin <martin@hotmail.com.br>
2024-03-01 16:32:01 -0300

    feat: added initial setup of accounts on redis to mirror database
    Signed-off-by: Martin <martin@hotmail.com.br>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,13 @@
 use deadpool_postgres::{ManagerConfig, Pool, RecyclingMethod, Runtime};
 use listenfd::ListenFd;
+use redis::aio::ConnectionManager;
 use redis::Client;
+use rinha_de_backend::application::cache::AccountCache;
+use rinha_de_backend::application::repositories::TransactionRepository;
 use rinha_de_backend::application::ServerData;
 use rinha_de_backend::infrastructure::server_impl::server::{match_routes, parse_http};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, UnixListener};
+use tokio::net::TcpListener;
 use tokio_postgres::NoTls;
 
 #[global_allocator]
@@ -28,7 +31,25 @@ async fn setup_pgsql() -> Pool {
     cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap()
 }
 
+async fn setup_redis(pool: &Pool) -> ConnectionManager {
+    let conn = ConnectionManager::new(Client::open("redis://localhost:6379").unwrap())
+        .await
+        .unwrap();
+
+    let repo = TransactionRepository { conn: pool.clone() };
+
+    let cache = AccountCache {
+        re_conn: conn.clone(),
+    };
+    for acc in repo.get_accounts().await {
+        cache.save_account(acc.id, &acc, None).await;
+    }
+    conn
+}
+
 async fn run() {
+    console_subscriber::init();
+
     let mut listenfd = ListenFd::from_env();
     let socket = if let Some(listener) = listenfd.take_tcp_listener(0).unwrap() {
         listener.set_nonblocking(true).unwrap();
@@ -38,22 +59,16 @@ async fn run() {
         TcpListener::bind("localhost:1337").await.unwrap()
     };
 
-    let re_conn =
-        redis::aio::ConnectionManager::new(Client::open("redis://localhost:6379").unwrap())
-            .await
-            .unwrap();
-
     let pg_pool = setup_pgsql().await;
+    let re_conn = setup_redis(&pg_pool).await;
 
-    let data = ServerData {
-        re_conn: re_conn,
-        pg_pool: pg_pool,
-    };
+    let data = ServerData { re_conn, pg_pool };
 
     println!("Server is running!");
 
     loop {
-        let (mut socket, other) = socket.accept().await.unwrap();
+        let (mut stream, _) = socket.accept().await.unwrap();
+        stream.set_nodelay(true).unwrap();
 
         let data = data.clone();
         tokio::spawn(async move {
@@ -61,7 +76,7 @@ async fn run() {
             let mut buf = [0; 1024];
 
             loop {
-                let read_bytes = match socket.read(&mut buf).await {
+                let read_bytes = match stream.read(&mut buf).await {
                     Ok(n) => n,
                     Err(e) => {
                         eprintln!("failed to read from socket; err = {:?}", e);
@@ -80,7 +95,8 @@ async fn run() {
                 } else {
                     response.unwrap_right()
                 };
-                socket.write_all(&response.into_http()).await.unwrap();
+                stream.write_all(&response.into_http()).await.unwrap();
+                // stream.shutdown().await.unwrap();
             }
         });
     }
